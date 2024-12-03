4343bf5aef20989fa82446ef142cb02565038014
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-12-03 11:42:28 +0700

    awokaowk

--- gito.py
+++ gito.py
@@ -14,12 +14,37 @@ def generate_random_number(length):
     end = (10**length) - 1
     return random.randint(start, end)
 
+def get_user_agent():
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    ]
+    return random.choice(user_agents)
 
+def login(email, passw):
+    data_payload_login = {
+        "username": email,
+        "password": passw,
+    }
+    data_pos_login = {
+        'data': json.dumps(data_payload_login),
+        'language': 'en'
+    }
+    p = requests.post('https://coreapi.gtiobitc.com/public/society/common/progress/DBlogin', data=data_pos_login)
+    p.raise_for_status()
+    try:
+        return p.json()['data']['affiliate_id']
+    except:
+        return 'BH5SCD'
+    
 error =0
 i = 0
 state = True
+sponsor = 'BH5SCD'
 while state:
     try:
+        user_agent = get_user_agent()
         a = generate_random_string(5)
         b = generate_random_string(5)
         email = str(a) +'.'+str(b)+'@gmail.com'
@@ -29,19 +54,12 @@ while state:
             "username": email,
             "password": passw,
             "confirm_password": passw,
-            "sponsor": "RR69DQ"
+            "sponsor": sponsor
         }
-
         data_pos = {
             'data': json.dumps(data_payload),
             'language': 'en'
         }
-        user_agents = [
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        ]
-        user_agent = random.choice(user_agents)
         p = requests.post('https://coreapi.gtiobitc.com/public/society/common/register/DBadd', data=data_pos, headers={
             "User-Agent": user_agent,
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -50,10 +68,16 @@ while state:
             "Referer": "https://google.com"
         })
         p.raise_for_status()
-        print(p.text)
-        print(f"p: {p.status_code}, ke {i}, email: {email}")
+        try:
+            p.text.index('Registration Success')
+            message = 'success'
+        except:
+            message = 'gagal'
+        # print(p.text)
+        print(f"p: {p.status_code}, ke {i}, email: {email}, sponsor: {sponsor}, status: {message}")
         error = 0
-        time.sleep(random.uniform(3, 10))
+        sponsor = login(email, passw)
+#        time.sleep(random.uniform(3, 10))
     except KeyboardInterrupt:
         print('Stopped by user!')
         state = False
